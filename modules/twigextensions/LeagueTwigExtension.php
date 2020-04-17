7cc7fb53ade8a7d3810f4ca0cf47e41ed95a0554
Billy Fagan <billy@the-kid.org>
2020-04-17 22:40:50 +0100

    clean up leaderboard

--- modules/twigextensions/LeagueTwigExtension.php
+++ modules/twigextensions/LeagueTwigExtension.php
@@ -2,99 +2,27 @@
 
 namespace modules\twigextensions;
 
-use craft\elements\db\ElementQuery;
 use craft\elements\Entry;
 use Twig\Extension\AbstractExtension;
-use Twig\TwigFunction;
 
 class LeagueTwigExtension extends AbstractExtension
 {
-  /** @var ElementQuery */
-  public $gamesQuery;
-  public $gamesPlayed;
-  public $homeGames;
-  public $awayGames;
-  public $homeLegsFor;
-  public $awayLegsFor;
-  public $homeLegsAgainst;
-  public $awayLegsAgainst;
-  public $homeGamesWon;
-  public $awayGamesWon;
-  public $players;
-  public $leaderboard;
-  public $totalLegsFor;
-  public $totalGamesPlayed;
-  public $totalLegsAgainst;
-  /**
-   * @var int|string
-   */
-  public $totalGamesWon;
+
+  private $gamesQuery;
+  private $players;
+
 
   public function __construct()
   {
-    $this->gamesQuery  = Entry::find()->section('games')->with(['player1', 'player2']);
-    $this->players     = Entry::find()->section('players')->all();
-    $this->leaderboard = [];
-    foreach ($this->players as $player)
-    {
-      $gamesQuery             = $this->gamesQuery;
-      $this->homeGames        = clone ($gamesQuery)->relatedTo([
-          'element' => $player,
-          'field'   => 'player1',
-      ]);
-      $this->awayGames        = clone ($gamesQuery)->relatedTo([
-          'element' => $player,
-          'field'   => 'player2',
-      ]);
-      $this->gamesPlayed      = clone ($gamesQuery)->relatedTo($player);
-      $this->totalGamesPlayed = $this->gamesPlayed->count();
-      $this->homeLegsFor      = $this->homeGames->sum('field_player1LegsWon');
-      $this->homeLegsAgainst  = $this->homeGames->sum('field_player2LegsWon');
-      $this->awayLegsFor      = $this->awayGames->sum('field_player2LegsWon');
-      $this->awayLegsAgainst  = $this->awayGames->sum('field_player1LegsWon');
-      $this->totalLegsFor     = $this->homeLegsFor + $this->awayLegsFor;
-      $this->totalLegsAgainst = $this->homeLegsAgainst + $this->awayLegsAgainst;
-      $this->homeGamesWon     = $this->homeGames->where('field_player1LegsWon > field_player2LegsWon')->count();
-      $this->awayGamesWon     = $this->awayGames->where('field_player2LegsWon > field_player1LegsWon')->count();
-      $this->totalGamesWon    = $this->homeGamesWon + $this->awayGamesWon;
-      $this->leaderboard[]    = (object)[
-          'playerUrl'        => $player->url,
-          'playerName'       => $player->title,
-          'totalGamesWon'    => $this->totalGamesWon,
-          'totalGamesPlayed' => $this->totalGamesPlayed,
-          'totalLegsFor'     => $this->totalLegsFor,
-          'totalLegsAgainst' => $this->totalLegsAgainst,
-      ];
-
-      usort($this->leaderboard, function ($a, $b) {
-        // if the player has won more games they're higher
-        if ($a->totalGamesWon !== $b->totalGamesWon)
-        {
-          return $a->totalGamesWon <=> $b->totalGamesWon;
-          // if they've won the same number of games, check the leg difference
-        } elseif (($a->totalLegsFor - $a->totalLegsAgainst) !== ($b->totalLegsFor - $b->totalLegsAgainst))
-        {
-          return ($a->totalLegsFor - $a->totalLegsAgainst) <=> ($b->totalLegsFor - $b->totalLegsAgainst);
-        }
-
-        // if the leg difference is the same, check the games played (games in hand)
-        return ($a->totalGamesPlayed <=> $b->totalGamesPlayed);
-
-      });
-      $this->leaderboard = array_reverse($this->leaderboard);
-    }
-  }
-
-  public function getFunctions()
-  {
-    return new TwigFunction('position', [$this, 'getPosition']);
+    $this->gamesQuery = Entry::find()->section('games')->with(['player1', 'player2']);
+    $this->players    = Entry::find()->section('players')->all();
   }
 
   public function getPosition($player)
   {
-    for ($i = 0; $i <= count($this->leaderboard); $i++)
+    for ($i = 0; $i <= count($this->getLeaderboard()); $i++)
     {
-      $leaguePlayer = $this->leaderboard[$i];
+      $leaguePlayer = $this->getLeaderBoard()[$i];
       if ($leaguePlayer->playerName === $player->title)
       {
         return $i + 1 . date("S", date_timestamp_get(date_create("2000-01-" . ($i + 1))));
@@ -107,8 +35,10 @@ class LeagueTwigExtension extends AbstractExtension
 
   public function mostPlayed($player)
   {
+    $gamesQuery = Entry::find()->section('games')->with(['player1', 'player2']);
+
     $opponents = [];
-    foreach ($this->gamesQuery->relatedTo($player)->all() as $game)
+    foreach ($gamesQuery->relatedTo($player)->all() as $game)
     {
       $opponents[] = (($game->player1[0]->id === $player->id) ? ("<a class='underline' href='{$game->player2[0]->url}'>" . $game->player2[0]->title) : ("<a class='underline' href='{$game->player1[0]->url}'>" . $game->player1[0]->title)) . "</a>";
     }
@@ -160,5 +90,62 @@ class LeagueTwigExtension extends AbstractExtension
     return array_merge(...$response);
   }
 
+
+  public function getLeaderboard()
+  {
+    $gamesQuery = Entry::find()->section('games')->with(['player1', 'player2']);
+    $players    = Entry::find()->section('players')->all();
+
+    $leaderboard = [];
+    foreach ($players as $player)
+    {
+      $homeGames        = clone ($gamesQuery)->relatedTo([
+          'element' => $player,
+          'field'   => 'player1',
+      ]);
+      $awayGames        = clone ($gamesQuery)->relatedTo([
+          'element' => $player,
+          'field'   => 'player2',
+      ]);
+      $gamesPlayed      = clone ($gamesQuery)->relatedTo($player);
+      $totalGamesPlayed = $gamesPlayed->count();
+      $homeLegsFor      = $homeGames->sum('field_player1LegsWon');
+      $homeLegsAgainst  = $homeGames->sum('field_player2LegsWon');
+      $awayLegsFor      = $awayGames->sum('field_player2LegsWon');
+      $awayLegsAgainst  = $awayGames->sum('field_player1LegsWon');
+      $totalLegsFor     = $homeLegsFor + $awayLegsFor;
+      $totalLegsAgainst = $homeLegsAgainst + $awayLegsAgainst;
+      $homeGamesWon     = $homeGames->where('field_player1LegsWon > field_player2LegsWon')->count();
+      $awayGamesWon     = $awayGames->where('field_player2LegsWon > field_player1LegsWon')->count();
+      $totalGamesWon    = $homeGamesWon + $awayGamesWon;
+      $leaderboard[]    = (object)[
+          'playerUrl'        => $player->url,
+          'playerName'       => $player->title,
+          'totalGamesWon'    => $totalGamesWon,
+          'totalGamesPlayed' => $totalGamesPlayed,
+          'totalLegsFor'     => $totalLegsFor,
+          'totalLegsAgainst' => $totalLegsAgainst,
+      ];
+
+      usort($leaderboard, function ($a, $b) {
+        // if the player has won more games they're higher
+        if ($a->totalGamesWon !== $b->totalGamesWon)
+        {
+          return $a->totalGamesWon <=> $b->totalGamesWon;
+          // if they've won the same number of games, check the leg difference
+        } elseif (($a->totalLegsFor - $a->totalLegsAgainst) !== ($b->totalLegsFor - $b->totalLegsAgainst))
+        {
+          return ($a->totalLegsFor - $a->totalLegsAgainst) <=> ($b->totalLegsFor - $b->totalLegsAgainst);
+        }
+
+        // if the leg difference is the same, check the games played (games in hand)
+        return ($a->totalGamesPlayed <=> $b->totalGamesPlayed);
+
+      });
+
+    }
+
+    return array_reverse($leaderboard);
+  }
 
 }
