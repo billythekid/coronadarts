0951af1fd10ca47bcec21938f366f1c85eed6d1f
billythekid <billythekid@github.com>
2025-09-03 15:02:30 +0100

    update config for craft 4 https://craftcms.com/docs/4.x/upgrade.html#configuration

--- config/general.php
+++ config/general.php
@@ -8,23 +8,19 @@
  */
 
 return [
-  // Global settings
-    '*'          => [
-      // Default Week Start Day (0 = Sunday, 1 = Monday...)
-        'defaultWeekStartDay'  => 1,
+    // Global settings
+    '*' => [
+        // Default Week Start Day (0 = Sunday, 1 = Monday...)
+        'defaultWeekStartDay' => 1,
 
-      // Whether generated URLs should omit "index.php"
+        // Whether generated URLs should omit "index.php"
         'omitScriptNameInUrls' => true,
 
-      // Control Panel trigger word
-        'cpTrigger'            => 'admin',
+        // Control Panel trigger word
+        'cpTrigger' => 'admin',
 
-      // The secure key Craft will use for hashing and encrypting data
-        'securityKey'          => getenv('SECURITY_KEY'),
-
-      // Whether to save the project config out to config/project.yaml
-      // (see https://docs.craftcms.com/v3/project-config.html)
-        'useProjectConfigFile' => true,
+        // The secure key Craft will use for hashing and encrypting data
+        'securityKey' => getenv('SECURITY_KEY'),
 
         'aliases' => [
             '@web' => getenv('default_site_url'),
@@ -32,21 +28,21 @@ return [
         ],
     ],
 
-  // Dev environment settings
-    'dev'        => [
-      // Dev Mode (see https://craftcms.com/guides/what-dev-mode-does)
+    // Dev environment settings
+    'dev' => [
+        // Dev Mode (see https://craftcms.com/guides/what-dev-mode-does)
         'devMode' => true,
     ],
 
-  // Staging environment settings
-    'staging'    => [
-      // Set this to `false` to prevent administrative changes from being made on staging
+    // Staging environment settings
+    'staging' => [
+        // Set this to `false` to prevent administrative changes from being made on staging
         'allowAdminChanges' => false,
     ],
 
-  // Production environment settings
+    // Production environment settings
     'production' => [
-      // Set this to `false` to prevent administrative changes from being made on production
+        // Set this to `false` to prevent administrative changes from being made on production
         'allowAdminChanges' => false,
 
     ],
